1b3f287946ac10a3f450fcbc430e5bc9795b9d42
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2023-07-01 20:40:35 +0700

    Update migration_dag.py

--- dags/migration_dag.py
+++ dags/migration_dag.py
@@ -2,32 +2,54 @@ from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-#from astronomer.providers.snowflake.operators.snowflake import SnowflakeOperatorAsync
+
 
 SNOWFLAKE_CONN_ID = "snowflake_default"
 
+# This function extracts the data from the specified table in the sqlite database.
 def extract_table(table_name, **kwargs):
-    # connecting the sql
+
+    # connects to the sqlite database and creates a cursor object.
     import sqlite3
     con = sqlite3.connect('include/sample.db')
     cur = con.cursor()
+
+    #executes the query to select all data from the specified table.
     cur.execute(f'SELECT * FROM {table_name}')
+
+    # Store the results of the query in a variable.
     table_data = cur.fetchall()
-    print(type(table_data))
-    for row in table_data:
-        print(row)
+
+    # closes the cursor object and datbase connection.
     cur.close()
     con.close()
+
+    # gets the task instance object from the kwargs dictionary.
     ti = kwargs['ti']
+
+    # pushes the table data to the XCOM store with the specified key.
     ti.xcom_push(key=f'table_data_{table_name}', value=table_data)
 
+# transforms the data from the specified table in the sqlite database
 def transform(table_name, **kwargs):
+
+    # gets the task instance object from the kwargs dictionary
     ti = kwargs['ti']
+
+    # gets the table data from the XCOM store.
     table_data = ti.xcom_pull(task_ids=f'extract_{table_name}', key=f'table_data_{table_name}')
+
+    # creates a query to insert the data into the specified table
     query = f'insert into {table_name} values'
+
+    # iterates over the table data and adds each row to the query.
     for row in table_data:
+        # remove the square bracket
         query += f" ({str(row)[1:-1]}),"
+    
+    # adds a semicolon to the end of the query.
     query = query[:-1] + ';'
+    
     return query
 
 with DAG(
